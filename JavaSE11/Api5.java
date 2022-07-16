d60eba06d7cfacb808ff4d5e0f9a37cadf72d939
mest-yamaru <83887766+mest-yamaru@users.noreply.github.com>
2022-07-16 19:41:27 +0900

    更新

---
+++ JavaSE11/Api5.java
@@ -0,0 +1,11 @@
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+public class Api5 {
+    public static void main(String[] args) {
+        LocalDate a = LocalDate.of(2019,8,19);
+        LocalDate b = LocalDate.now();
+        b.with(DayOfWeek.MONDAY);
+        System.out.println(a.equals(b) + ", " + a.isBefore(b));
+    }
+}
