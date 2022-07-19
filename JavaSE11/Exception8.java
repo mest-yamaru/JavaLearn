2d61030723f6393e1856a2f59e0de84aa18d7885
mest-yamaru <83887766+mest-yamaru@users.noreply.github.com>
2022-07-19 09:29:19 +0900

    更新

--- JavaSE11/Exception8.java
+++ JavaSE11/Exception8.java
@@ -1,3 +1,11 @@
 public class Exception8 {
-    
+    public static void main(String[] args) {
+        try {
+            System.out.println("A");
+        } finally {
+            System.out.println("B");
+        } finally {
+            System.out.println("C");
+        }
+    }
 }
