ad08e5ffd35f3904efc52063f93a50f05cc461d7
mest-yamaru <83887766+mest-yamaru@users.noreply.github.com>
2022-07-24 22:51:40 +0900

    更新

--- JavaSE11/second/Data/Data12.java
+++ JavaSE11/second/Data/Data12.java
@@ -1,6 +1,6 @@
 public class Data12 {
     public static void main(String[] args) {
         String str = "abcde";
-        System.out.println(str.indexOf("abcdef")); //引数に指定した文字がどの位置に存在するか調べるメソッド0から
+        System.out.println(str.indexOf("abcdef")); //引数に指定した文字がどの位置に存在するか調べるメソッド0から、存在しない場合は-1を戻す
     }
 }
